525c00dacdb9df292f86b2b4b38853d90a1f53a5
toolzgithub <zpctools@163.com>
2016-07-25 17:42:17 +0800

    第一次提交

---
+++ src/net/sunniwell/appmanager/activity/MainActivity.java
@@ -0,0 +1,108 @@
+package net.sunniwell.appmanager.activity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import net.sunniwell.appmanager.R;
+import net.sunniwell.appmanager.R.drawable;
+import net.sunniwell.appmanager.R.id;
+import net.sunniwell.appmanager.R.layout;
+
+/**
+ * 软件管理器的主类，负责加载应用列表，卸载应用功能
+ * 
+ * @author 郑鹏超
+ * @时间 2016年7月25号 09:40
+ */
+public class MainActivity extends Activity {
+
+	private ListView mLvMain;// 应用程序列表
+	private List<String> test = new ArrayList<>();// 测试数据
+	private AppManagerAdapter mAppManagerAdapter;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		initView();
+		initData();
+	}
+
+	private void initView() {
+		mLvMain = (ListView) findViewById(R.id.lv_main);
+
+	}
+
+	private void initData() {
+		//测试数据
+		for (int i = 0; i < 50; i++) {
+			test.add("这是测试数据第"+i+"条");
+		}
+		mAppManagerAdapter = new AppManagerAdapter(test);
+		mLvMain.setAdapter(mAppManagerAdapter);
+	}
+
+	/**
+	 * 应用列表适配器
+	 * 
+	 */
+	private class AppManagerAdapter extends BaseAdapter {
+
+		private List<String> appInfos;
+
+		public AppManagerAdapter(List<String> appInfos) {
+			this.appInfos = appInfos;
+		}
+
+		@Override
+		public int getCount() {
+			return appInfos.size();
+		}
+
+		@Override
+		public String getItem(int position) {
+			return appInfos.get(position);
+		}
+
+		@Override
+		public long getItemId(int position) {
+			return position;
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			if(convertView == null){
+				convertView = View.inflate(MainActivity.this, R.layout.itme_lv_main, null);
+				AppManagerViews appManagerViews = new AppManagerViews();
+				appManagerViews.mIvAppManagerIcon = (ImageView) convertView.findViewById(R.id.iv_app_manager_icon);
+				appManagerViews.mTvAppManagerName = (TextView) convertView.findViewById(R.id.tv_app_manager_name);
+				appManagerViews.mIvAppManagerIcon.setImageResource(R.drawable.ic_launcher);  
+				appManagerViews.mTvAppManagerName.setText(getItem(position));
+				convertView.setTag(appManagerViews);
+			}else{
+				AppManagerViews appManagerViews = (AppManagerViews) convertView.getTag();  
+				appManagerViews.mIvAppManagerIcon.setImageResource(R.drawable.ic_launcher);  
+				appManagerViews.mTvAppManagerName.setText(getItem(position));
+			}
+			return convertView;
+		}
+
+	}
+	/**
+	 * 用来优化listview的类 
+	 *
+	 */
+	private class AppManagerViews{
+		ImageView mIvAppManagerIcon;
+		TextView mTvAppManagerName;
+	}
+}
---
+++ src/net/sunniwell/appmanager/bean/AppInfo.java
@@ -0,0 +1,31 @@
+package net.sunniwell.appmanager.bean;
+
+import android.graphics.drawable.Drawable;
+
+/**
+ * 应用程序类，包括了程序相关属性
+ * 
+ * @author 郑鹏超
+ * @时间 2016年7月25号 16:30
+ */
+public class AppInfo {
+	private Drawable icon;
+	private String appName;
+
+	public Drawable getIcon() {
+		return icon;
+	}
+
+	public void setIcon(Drawable icon) {
+		this.icon = icon;
+	}
+
+	public String getAppName() {
+		return appName;
+	}
+
+	public void setAppName(String appName) {
+		this.appName = appName;
+	}
+
+}
