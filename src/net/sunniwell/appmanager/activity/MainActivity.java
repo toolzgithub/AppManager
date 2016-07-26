c39c02f3304dedb11ba27cb5bb806cedf33da044
toolzgithub <zpctools@163.com>
2016-07-26 11:52:16 +0800

    把应用信息加载到列表中

--- src/net/sunniwell/appmanager/activity/MainActivity.java
+++ src/net/sunniwell/appmanager/activity/MainActivity.java
@@ -12,9 +12,8 @@ import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 import net.sunniwell.appmanager.R;
-import net.sunniwell.appmanager.R.drawable;
-import net.sunniwell.appmanager.R.id;
-import net.sunniwell.appmanager.R.layout;
+import net.sunniwell.appmanager.bean.AppInfo;
+import net.sunniwell.appmanager.engine.AppInfoProvider;
 
 /**
  * 软件管理器的主类，负责加载应用列表，卸载应用功能
@@ -23,9 +22,10 @@ import net.sunniwell.appmanager.R.layout;
  * @时间 2016年7月25号 09:40
  */
 public class MainActivity extends Activity {
- 
+
 	private ListView mLvMain;// 应用程序列表
-	private List<String> test = new ArrayList<>();// 测试数据
+	/* private List<String> test = new ArrayList<>();// 测试数据 */
+	private List<AppInfo> mAppInfos = new ArrayList<>();//应用信息对象
 	private AppManagerAdapter mAppManagerAdapter;
 
 	@Override
@@ -55,10 +55,12 @@ public class MainActivity extends Activity {
 	 */
 	private void initData() {
 		// 测试数据
-		for (int i = 0; i < 50; i++) {
-			test.add("这是测试数据第" + i + "条");
-		}
-		mAppManagerAdapter = new AppManagerAdapter(test);
+		/*
+		 * for (int i = 0; i < 50; i++) { test.add("这是测试数据第" + i + "条"); }
+		 * mAppManagerAdapter = new AppManagerAdapter(test);
+		 */
+		mAppInfos = AppInfoProvider.getAppInfo(getApplicationContext());
+		mAppManagerAdapter = new AppManagerAdapter(mAppInfos);
 		mLvMain.setAdapter(mAppManagerAdapter);
 	}
 
@@ -69,15 +71,15 @@ public class MainActivity extends Activity {
 	 */
 	private class AppManagerAdapter extends BaseAdapter {
 
-		private List<String> appInfos;
-		
+		private List<AppInfo> appInfos;
+
 		/**
 		 * 功能：构造方法用来传递要显示的数据
 		 * 
 		 * @author 郑鹏超
 		 * @时间 2016年7月25日
 		 */
-		public AppManagerAdapter(List<String> appInfos) {
+		public AppManagerAdapter(List<AppInfo> appInfos) {
 			this.appInfos = appInfos;
 		}
 
@@ -87,7 +89,7 @@ public class MainActivity extends Activity {
 		}
 
 		@Override
-		public String getItem(int position) {
+		public AppInfo getItem(int position) {
 			return appInfos.get(position);
 		}
 
@@ -98,18 +100,19 @@ public class MainActivity extends Activity {
 
 		@Override
 		public View getView(int position, View convertView, ViewGroup parent) {
+			AppInfo appInfo = getItem(position);
 			if (convertView == null) {
 				convertView = View.inflate(MainActivity.this, R.layout.itme_lv_main, null);
 				AppManagerViews appManagerViews = new AppManagerViews();
 				appManagerViews.mIvAppManagerIcon = (ImageView) convertView.findViewById(R.id.iv_app_manager_icon);
 				appManagerViews.mTvAppManagerName = (TextView) convertView.findViewById(R.id.tv_app_manager_name);
-				appManagerViews.mIvAppManagerIcon.setImageResource(R.drawable.ic_launcher);
-				appManagerViews.mTvAppManagerName.setText(getItem(position));
+				appManagerViews.mIvAppManagerIcon.setImageDrawable(appInfo.getIcon());
+				appManagerViews.mTvAppManagerName.setText(appInfo.getAppName());
 				convertView.setTag(appManagerViews);
 			} else {
 				AppManagerViews appManagerViews = (AppManagerViews) convertView.getTag();
-				appManagerViews.mIvAppManagerIcon.setImageResource(R.drawable.ic_launcher);
-				appManagerViews.mTvAppManagerName.setText(getItem(position));
+				appManagerViews.mIvAppManagerIcon.setImageDrawable(appInfo.getIcon());
+				appManagerViews.mTvAppManagerName.setText(appInfo.getAppName());
 			}
 			return convertView;
 		}
